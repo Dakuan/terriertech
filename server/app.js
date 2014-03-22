12bbb635dd10256dec303a1dbe96da084897e637
Dakuan <dom.barker808@gmail.com>
2014-03-22 08:37:30 +0000

    use correct host

--- server/app.js
+++ server/app.js
@@ -1,16 +1,12 @@
 module.exports = function () {
     var express = require('express'),
+        uglifyMiddleware = require('express-uglify-middleware'),
         lessMiddleware = require('less-middleware'),
         path = require('path'),
         app = express();
 
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
-
-    // app.use("/public/javascripts", express.static(path.join(__dirname, "../client")));
-
-
-    var uglifyMiddleware = require('express-uglify-middleware');
 
     app.use(uglifyMiddleware({
         src: path.join('', __dirname, '../client'),
@@ -32,7 +28,9 @@ module.exports = function () {
     app.use("/public", express.static(__dirname + "/public"));
     app.disable('x-powered-by');
     app.get('/', function (req, res) {
-        res.render('index/index');
+        res.render('index/index', {
+            host: req.host
+        });
     });
     var port = Number(process.env.PORT || 5000);
     console.log('express is booting on port ' + port);
